2c36a638dfb66febe40811164603fe934fcdc66c
Wladmyr Almeida <wladmyralmeida@gmail.com>
2018-07-24 21:43:51 -0300

    Página de Perfil

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,8 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Home', component: 'HomePage'}
+      { title: 'Perfil', component: 'ProfilePage'},
+      { title: 'Categorias', component: 'CategoriasPage'}
     ];
 
   }
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -4,9 +4,12 @@ import { Injectable } from "../../node_modules/@angular/core";
 import { HttpClient } from '../../node_modules/@angular/common/http';
 import { API_CONFIG } from '../config/api.config';
 import { StorageService } from './storage.service';
+import { JwtHelper } from 'angular2-jwt';
 
 @Injectable()
 export class AuthService{
+
+    jwtHelper : JwtHelper = new JwtHelper();
 
     constructor(public http: HttpClient, public storage : StorageService){
 
@@ -23,7 +26,8 @@ export class AuthService{
     successfulLogin(authorizationValue : string){
         let tok = authorizationValue.substring(7);
         let user : LocalUser = {
-            token : tok
+            token : tok,
+            email : this.jwtHelper.decodeToken(tok).sub
         };
         this.storage.setLocalUser(user);
     }
